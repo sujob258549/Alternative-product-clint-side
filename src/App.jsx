85ef1a807b13496aa270b3ec1dbfc84528bd4b7b
DevSujon <sujonmaia274@gmail.com>
2024-05-11 00:44:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1 className='text-xl'>hellow word</h1>
+   </div>
+  )
+}
+
+export default App
---
+++ src/Router/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom"
+import Roots from "../Common/Roots"
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Roots></Roots>
+    }
+])
+
+
+
+
+export default router;
