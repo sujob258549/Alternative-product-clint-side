ff72b25b61a5744719bdcc0e9aec6e7f22d65587
DevSujon <sujonmaia274@gmail.com>
2024-05-13 10:48:24 +0600

    dainamic laout

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+vercel - https://assinment-11-defarent-product-servire.vercel.app
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+live link : https://alternative-product-11.web.app
--- src/assets/AddQueryDetailquerySizquery/Allproduct.jsx
+++ src/assets/AddQueryDetailquerySizquery/Allproduct.jsx
@@ -1,9 +1,11 @@
 
 import { useEffect, useState } from "react";
 import './style.css'
+import { Link } from "react-router-dom";
 
 const Allproduct = () => {
     const [sixDatas, setSixdatas] = useState([])
+    const [gridTogle, setgridTogle] = useState(3)
     console.log(sixDatas)
 
     useEffect(() => {
@@ -14,13 +16,27 @@ const Allproduct = () => {
             })
     }, [])
 
+    const handelChengelaout = e => {
+        const griddata = e.target.value;
+        setgridTogle(griddata);
+    }
+
     return (
         <div className="pb-10 md:pb-20 container mx-auto">
             <div className="px-5 py-10 md:py-20 w-full md:w-[80%] lg:w-[70%] mx-auto space-y-5">
                 <h1 className="kurali-font text-3xl md:text-5xl font-bold text-center">All Query/Product</h1>
                 <p className="text-center text-[18px]">Products are the backbone of modern life, serving a multitude of purposes across various industries and sectors. From everyday essentials like food, clothing, and electronics to specialized tools and equipment used in manufacturing and construction, products play a vital role in meeting human needs and driving economic activity. Whether it.s the latest smartphone, a high-performance automobile, or life-saving medical devices, each product serves a specific function and contributes to enhancing our quality of life. Moreover, advancements in technology continue to revolutionize the way products are designed, produced, and distributed, leading to constant innovation and improvement across different sectors.</p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-5 md:px-0">
+            <div className="flex justify-center py-10">
+                <select onChange={handelChengelaout} className="text-xl kurali-font font-medium select select-info w-full max-w-xs">
+                    <option className="text-xl kurali-font font-medium" disabled selected>Chenge grid leout option</option>
+                    <option className="text-xl kurali-font font-medium" value={1}>Creat laout1</option>
+                    <option className="text-xl kurali-font font-medium" value={2}>Creat laout2</option>
+                    <option className="text-xl kurali-font font-medium" value={3}>Creat laout3</option>
+                    {/* <option className="text-xl kurali-font font-medium" value={4}>Creat laout4</option> */}
+                </select>
+            </div>
+            <div className={`grid  md:grid-cols-${gridTogle} gap-10 px-5 md:px-0`}>
                 {
                     sixDatas.map(product => (
                         <div key={product._id} className=' flex flex-col justify-between shadow rounded-lg relative'>
@@ -40,10 +56,10 @@ const Allproduct = () => {
                                 </div>
                             </div>
                             <div className="">
-                                <button className='b-radis inline-flex items-center justify-center  bg-green-600 py-3 px-6 text-xl  font-dm  font-semibold text-white shadow-xl shadow-green-600/85 transition-transform duration-200 ease-in-out  w-full'>
+                                <Link to={`/detail-recomented/${product._id}`} className='b-radis inline-flex items-center justify-center  bg-green-600 py-3 px-6 text-xl  font-dm  font-semibold text-white shadow-xl shadow-green-600/85 transition-transform duration-200 ease-in-out  w-full'>
                                     Rrecommend Product
 
-                                </button>
+                                </Link>
                             </div>
 
                         </div>
